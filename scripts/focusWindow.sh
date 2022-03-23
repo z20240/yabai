a7e9f907966c4f42d0b65df6a0b8a3713a148dd9
les <z20240z@gmail.com>
2022-03-24 01:02:19 +0800

    fix bug

--- scripts/focusWindow.sh
+++ scripts/focusWindow.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 inputAction=$1
 is_zoom_fullscree=$(yabai -m query --windows --window | jq -re '."zoom-fullscreen"')
+is_zoom_fullscree_new=$(yabai -m query --windows --window | jq -re '."has-fullscreen-zoom"')
 is_floating=$(yabai -m query --windows --window | jq -re ".floating")
+is_floating_new=$(yabai -m query --windows --window | jq -re '."is-floating"')
 
 # if window is in fullscreen, toggle to shrink it.
-if [ $is_zoom_fullscree -eq "1" ] ; then
+if [[ "$is_zoom_fullscree" == "1" || "$is_zoom_fullscree_new" == "true" ]] ; then
   yabai -m window --toggle zoom-fullscreen
 fi
 
-if [ $is_floating -eq "1" ]; then
+if [[ "$is_floating" == "1" || "$is_floating_new" == "true" ]] ; then
   # do move for floating mode.
   case $inputAction in
     'left')
